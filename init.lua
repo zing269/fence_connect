411beaf1e00a642ab608d6c122be4f812f4a6198
zing269 <Zing269@gmx.com>
2017-06-13 21:58:03 -0400

    Create init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,7 @@
 -- adds a connect_to_fence group to various nodes and adds that group to the fence connect_to group list
+fence_connect = {}
+
+dofile(minetest.get_modpath("fence_connect") .. "/fence_connect.conf")
 
 local def = {}
 local connects_to = {}
@@ -32,67 +35,6 @@ if minetest.get_modpath("coloredwood") then
 end
 
 -- let fences connect to these nodes:
-add_connect_to_fence("default:stone")
-add_connect_to_fence("default:cobble")
-add_connect_to_fence("default:stonebrick")
-add_connect_to_fence("default:stone_block")
-add_connect_to_fence("default:mossycobble")
-add_connect_to_fence("default:desert_stone")
-add_connect_to_fence("default:desert_cobble")
-add_connect_to_fence("default:desert_stonebrick")
-add_connect_to_fence("default:desert_stone_block")
-add_connect_to_fence("default:sandstone")
-add_connect_to_fence("default:sandstonebrick")
-add_connect_to_fence("default:sandstone_block")
-add_connect_to_fence("default:desert_sandstone")
-add_connect_to_fence("default:desert_sandstone_brick")
-add_connect_to_fence("default:desert_sandstone_block")
-add_connect_to_fence("default:silver_sandstone")
-add_connect_to_fence("default:silver_sandstone_brick")
-add_connect_to_fence("default:silver_sandstone_block")
-add_connect_to_fence("default:obsidian")
-add_connect_to_fence("default:obsidianbrick")
-add_connect_to_fence("default:obsidian_block")
-add_connect_to_fence("default:dirt")
-add_connect_to_fence("default:dirt_with_grass")
-add_connect_to_fence("default:dirt_with_grass_footsteps")
-add_connect_to_fence("default:dirt_with_dry_grass")
-add_connect_to_fence("default:dirt_with_snow")
-add_connect_to_fence("default:dirt_with_rainforest_litter")
-
-
--- maybe
---add_connect_to_fence("default:sand")
---add_connect_to_fence("default:desert_sand")
---add_connect_to_fence("default:silver_sand")
---add_connect_to_fence("default:gravel")
---add_connect_to_fence("default:snow")
-
-add_connect_to_fence("default:clay")
-add_connect_to_fence("default:snowblock")
-add_connect_to_fence("default:ice")
-
-add_connect_to_fence("default:stone_with_coal")
-add_connect_to_fence("default:coalblock")
-add_connect_to_fence("default:stone_with_iron")
-add_connect_to_fence("default:steelblock")
-add_connect_to_fence("default:stone_with_copper")
-add_connect_to_fence("default:copperblock")
-add_connect_to_fence("default:stone_with_tin")
-add_connect_to_fence("default:tinblock")
-add_connect_to_fence("default:bronzeblock")
-add_connect_to_fence("default:stone_with_gold")
-add_connect_to_fence("default:goldblock")
-add_connect_to_fence("default:stone_with_mese")
-add_connect_to_fence("default:mese")
-add_connect_to_fence("default:stone_with_diamond")
-add_connect_to_fence("default:diamondblock")
-
-add_connect_to_fence("default:cactus")
-add_connect_to_fence("default:coral_brown")
-add_connect_to_fence("default:coral_orange")
-add_connect_to_fence("default:coral_skeleton")
-
-add_connect_to_fence("default:glass")
-add_connect_to_fence("default:obsidian_glass")
-add_connect_to_fence("default:brick")
+for _, name in pairs(fence_connect.connect_to_list) do
+    add_connect_to_fence(name)
+end
